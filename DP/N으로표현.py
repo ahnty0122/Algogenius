3ee01a0ddadff42180e6c45f42bce280ad340193
ahnty0122 <ahnty0122@naver.com>
2021-03-30 01:31:35 +0900

    210330

--- DP/N으로표현.py
+++ DP/N으로표현.py
@@ -1,3 +1,5 @@
+# 모든 N에 대한 결과 저장해놓기
+# i 횟수만큼 연산..
 def solution(N, number):
     possible_set = [0,[N]] # 연산 조합으로 나올 수 있는 가능한 숫자들
     if N == number: 
@@ -9,7 +11,6 @@ def solution(N, number):
         for i_half in range(1, i // 2 + 1): # 사용되는 숫자의 횟수 구할 때 절반 이상으로 넘어가면 같은 결과만 나오니깐 절반까지만 사용 
             for x in possible_set[i_half]:
                 for y in possible_set[i - i_half]: # x와 y를 더하면 i 가 되도록 만든 수다. 
-                    # print(possible_set)
                     case_set.append(x + y) # 각 사칙연산 결과를 더한다.
                     case_set.append(x - y)
                     case_set.append(y - x)
@@ -21,6 +22,6 @@ def solution(N, number):
             if number in case_set:
                 return i
             possible_set.append(case_set) # 최종 결과물 set에 사칙 연산 결과를 더한다.
-    return -1 #N 이 8까지 답이 없으면 -1을 출력한다.
+    return -1 #N 이 8까지 답이 없으면 -1을 출력
 
 print(solution(5, 12))
