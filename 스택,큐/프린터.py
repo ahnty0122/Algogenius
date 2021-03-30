8ab847fbad183786251207f9d28b00dba55c7d72
ahnty0122 <ahnty0122@naver.com>
2021-03-30 21:35:23 +0900

    210330

---
+++ 스택,큐/프린터.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    d = deque([(i, j) for j, i in enumerate(priorities)])
+    while d:
+        item = d.popleft()
+        print(item)
+        if d and max(d)[0] > item[0]:
+            d.append(item)
+        else:
+            answer += 1
+            if item[1] == location:
+                break
+    return answer
+
+print(solution([1, 1, 9, 1, 1, 1], 0))
+# 5
