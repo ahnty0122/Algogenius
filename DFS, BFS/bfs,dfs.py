76ce52ff1e0c65a56519f0f98f08a98d35c07380
ahnty0122 <ahnty0122@naver.com>
2021-08-21 11:41:48 +0900

    210821

---
+++ DFS, BFS/bfs,dfs.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+read = sys.stdin.readline
+
+n, m, v = map(int, read().split())
+graph = [[0 for i in range(n + 1)] for j in range(n + 1)]
+
+visited_d = [0] * (n + 1)
+visited_b = [0] * (n + 1)
+
+for i in range(m):
+    a, b = map(int, read().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+def dfs(visited, graph, v):
+    visited[v] = 1
+    print(v, end = " ")
+    for i in range(n + 1):
+        if visited[i] == 0 and graph[v][i] == 1:
+            dfs(visited, graph, i)
+
+def bfs(visited, graph, v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = 1
+    while(queue):
+        v = queue.popleft()
+        print(v, end = " ")
+        for i in range(n + 1):
+            if visited[i] == 0 and graph[v][i] == 1:
+                queue.append(i)
+                visited[i] = 1
+
+dfs(visited_d, graph, v)
+print()
+bfs(visited_b, graph, v)
---
+++ DFS, BFS/미로탐색.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+matrix = [stdin.readline().rstrip() for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 경로 탐색
+# queue 방식 사용
+queue = [(0,0)]
+visited[0][0] = 1
+
+while queue:
+    x, y = queue.pop(0)
+
+    if x == N-1 and y == M-1:
+        # 최종 경로 도착
+        print(visited[x][y])
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if visited[nx][ny] == 0 and matrix[nx][ny] == '1':
+                visited[nx][ny] = visited[x][y] + 1
+                queue.append((nx,ny))
